41663e0c4ec28ddf1da47f8824bade0873913e35
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-04 20:25:39 +0100

    feat: use waitUntil

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-export interface Env {
+export type Env = {
 	HOOK_URLS: string;
-}
+};
 
 export default {
-	async scheduled(event: ScheduledEvent, env: Env): Promise<void> {
+	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
 		const urls = env.HOOK_URLS.split(';');
-		const promises = urls.map((url) => fetch(url, { method: 'POST' }));
-
-		for await (const resp of promises) {
-			const wasSuccessful = resp.ok ? 'success' : 'fail';
-			console.log(`trigger fired at ${event.cron}: ${wasSuccessful}\n url: ${resp.url}`);
-		}
+		const promises = urls.map(async (url) => {
+			const res = await fetch(url, { method: 'POST' });
+			if (!res.ok) {
+				console.error(`Failed to trigger url: ${url}`);
+				throw new Error(`Failed to trigger url: ${url}`);
+			}
+			// eslint-disable-next-line no-console
+			console.log(`Triggered url: ${url}`);
+			return res;
+		});
+		ctx.waitUntil(Promise.all(promises));
 	},
 };
